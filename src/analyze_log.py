639f11f3aed292056500b4f099139f4fa82a81ed
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-09-22 15:45:49 -0300

    Feat: Requirement 1 complete

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,7 @@
 import csv
-from src.publicity_campaign import most_requested_dish
+from src.publicity_campaign import (
+    most_requested_dish, get_burger_orders,
+    dish_not_ordered, get_days)
 
 
 def analyze_log(path_to_file):
@@ -8,15 +10,19 @@ def analyze_log(path_to_file):
 
     try:
         with open(path_to_file, mode="r", encoding="utf-8") as file:
-            order_history = csv.reader(file, delimiter=",", quotechar='\n')
+            order_history = list(
+                csv.reader(file, delimiter=",", quotechar='\n'))
 
             result = [
                 most_requested_dish(order_history, "maria"),
+                get_burger_orders(order_history, "arnaldo", "hamburguer"),
+                dish_not_ordered(order_history, "joao"),
+                get_days(order_history, "joao")
             ]
 
         with open("data/mkt_campaign.txt", mode="w", encoding="utf-8") as file:
             for line in result:
-                file.write(f"{line}")
+                file.write(f"{line}\n")
 
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
--- src/publicity_campaign.py
+++ src/publicity_campaign.py
@@ -1,11 +1,44 @@
 from collections import Counter
 
 
-def most_requested_dish(dishes, client):
+def most_requested_dish(orders, client):
     requested_dish = list()
 
-    for name, dish, _ in dishes:
+    for name, dish, _ in orders:
         if name == client:
             requested_dish.append(dish)
         most_requested = Counter(requested_dish)
-    return max(most_requested)
+    return most_requested.most_common()[0][0]
+
+
+def get_burger_orders(orders, client, food):
+    requested_burgers = list()
+
+    for name, dish, _ in orders:
+        if name == client and dish == food:
+            requested_burgers.append(dish)
+    number_of_burgers = len(requested_burgers)
+    print(number_of_burgers)
+    return number_of_burgers
+
+
+def dish_not_ordered(orders, client):
+    get_dishes = set()
+    get_clients = set()
+
+    for name, dish, _ in orders:
+        get_dishes.add(dish)
+        if name == client:
+            get_clients.add(dish)
+    return get_dishes.difference(get_clients)
+
+
+def get_days(orders, client):
+    get_days = set()
+    get_clients = set()
+
+    for name, _, day in orders:
+        get_days.add(day)
+        if name == client:
+            get_clients.add(day)
+    return get_days.difference(get_clients)
